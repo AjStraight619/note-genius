3f840b84f988126707e7ce9cefdbaecbe8f8cfda
Alex Straight <compsciaj@outlook.com>
2023-11-20 11:43:00 -0800

    Adding react-spring animations

--- app/test/animations/page.tsx
+++ app/test/animations/page.tsx
@@ -2,13 +2,48 @@
 import { animated, useSpring } from "@react-spring/web";
 
 export default function MyComponent() {
-  const [props, api] = useSpring(
-    () => ({
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-    }),
-    []
-  );
+  // Define the spring for the animating lines
+  const [lineProps, setLineProps] = useSpring(() => ({
+    from: { x1: -100, x2: 100 },
+    to: async (next, cancel) => {
+      await next({ x1: 100, x2: -100 });
+      await next({ x1: -100, x2: 100 });
+    },
+    config: { duration: 2000 },
+    loop: true,
+  }));
 
-  return <animated.div style={props}>Hello World</animated.div>;
+  // Background grid style
+  const gridStyle = {
+    backgroundImage: `
+      repeating-linear-gradient(0deg, transparent, transparent 49px, #e5e7eb 50px),
+      repeating-linear-gradient(-90deg, transparent, transparent 49px, #e5e7eb 50px)
+    `,
+  };
+
+  return (
+    <div className="h-screen" style={gridStyle}>
+      <div className="grid grid-cols-3 gap-4 place-items-center h-full">
+        {/* Your cards go here */}
+      </div>
+      <animated.div
+        className="absolute top-0"
+        style={{
+          height: "2px",
+          background: "blue",
+          width: "100%",
+          transform: lineProps.x1.to((x1) => `translateX(${x1}%)`),
+        }}
+      />
+      <animated.div
+        className="absolute top-0"
+        style={{
+          height: "2px",
+          background: "blue",
+          width: "100%",
+          transform: lineProps.x2.to((x2) => `translateX(${x2}%)`),
+        }}
+      />
+    </div>
+  );
 }
